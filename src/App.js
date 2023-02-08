3146296046e606ec972bd0740e687041d7bcf283
Juttta <n1anju00@students.oamk.fi>
2023-02-08 12:52:27 +0200

    iik

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
+/* litra: pulloa * 0,33
+grams: litraa*8*4.5
+palaminen: paino/10
+grams(left): grams - (palaminen * aika)
+
+mies: grams/(paino*0.7)
+nainen: grams/(paino*0.6)*/
+
 import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react'
 
 function App() {
+
+  const [paino, setPaino] = useState(0)
+  const [pullot, setPullot] = useState(1)
+  const [suku, setSuku] = useState("male")
+  const [time, setTime] = useState(1)
+  const [result, setResult] = useState(0)
+
+  function laske(e) {
+    e.preventDefault();
+
+    const litra = pullot * 0.33
+    const grams = litra * 8 * 4.5
+    const palaminen = paino / 10
+    const gramsLeft = grams - (palaminen * time)
+    let result = 0
+
+
+    if (suku === "male") {
+      result = gramsLeft / (paino * 0.7)
+    }
+    
+    else {
+      result = gramsLeft / (paino * 0.6)
+    }
+
+    if (result < 0) {
+        result = 0
+    }
+
+    setResult(result.toFixed(1))
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={laske}>
+    <h1>Alkometri</h1>
+    <div>
+      <label>Weight</label>
+      <input type="number" name="paino" step="1"  onChange={e => setPaino(e.target.value)}/>
     </div>
+    <div>
+      <label>Bottles</label>
+      <select name="bottles" id="pullot" onChange={e => setPullot(e.target.value)}>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      </select>  
+    </div>
+    <div>
+      <label>Time</label>
+      <select name="time" id="aika" onChange={e => setTime(e.target.value)}>
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      </select>
+    </div>
+    <div>
+      <label>Gender</label>
+      <input type="radio" name="gender" id="male" value="male" onChange={e => setSuku("male")} defaultChecked />
+      <label>Male</label>
+      <input type="radio" name="gender" id="female" onChange={e => setSuku("female")}/>
+      <label>Female</label>
+    </div>´
+    <output>{result}</output>
+    <button>paina</button>
+    </form>
   );
 }
 
